d295272bde49468afc3cc1d3576a154a412c88c1
DeeBest <simphiwedladla8@gmail.com>
2024-03-26 20:28:46 +0200

    Final touch ups

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const navbarLinks = document.querySelector(".navbar-links");
 
-toggleBtn.addEventListener('click', ()=> {
+toggleBtn.addEventListener("click", () => {
   // Check the current display property of navbarLinks
   const currentDisplay = getComputedStyle(navbarLinks).display;
-  
+
   // Toggle the display property based on its current value
-  if (currentDisplay === 'none') {
-    navbarLinks.style.display = 'flex';
+  if (currentDisplay === "none") {
+    navbarLinks.style.display = "flex";
   } else {
-    navbarLinks.style.display = 'none';
+    navbarLinks.style.display = "none";
   }
 });
